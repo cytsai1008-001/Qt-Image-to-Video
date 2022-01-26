b31b34e81f8b5e374613e21104e60c61f67f490d
CYTsai <47322522+cytsai1008@users.noreply.github.com>
2022-01-26 15:11:20 +0800

    :zap: Add some features and fix thread lock

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import asyncio
 import datetime
 import os
 import subprocess
@@ -7,17 +6,75 @@ import time
 import traceback
 
 from PySide2 import QtWidgets, QtGui
+from PySide2.QtCore import QProcess
 from PySide2.QtGui import QFontDatabase
 
 from Main_Window import Ui_Form
-
 
 # from better_ffmpeg_progress import FfmpegProcess
 
 
 # supported_color_space = ['libx264 (最佳品質)', 'yuv420p (兼容性)']
+date = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
 
 
+def check_pyinstaller():
+    if getattr(sys, "frozen", False) and hasattr(sys, "_MEIPASS"):
+        print(
+            "[INFO] You are running from PyInstaller packed executable. Hopes there is no bug."
+        )
+        return True
+    else:
+        print("[INFO] You are running from normal Python source code.")
+        return False
+
+
+async def ffmpeg_process_rework(
+    resolution_w, resolution_h, frame_rate, input_dir, output_dir, file_format, fps
+):
+    if not check_pyinstaller():
+        process = subprocess.Popen(
+            [
+                "python",
+                "runner.py",
+                "--input",
+                input_dir,
+                "--output",
+                output_dir,
+                "--fps",
+                fps,
+                "--time",
+                f"{frame_rate}",
+                "--format",
+                file_format,
+            ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            shell=True,
+        )
+    else:
+        process = subprocess.Popen(
+            [
+                "runner.exe",
+                "--input",
+                input_dir,
+                "--output",
+                output_dir,
+                "--fps",
+                fps,
+                "--time",
+                f"{frame_rate}",
+                "--format",
+                file_format,
+            ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            shell=True,
+        )
+    return process.communicate()[1].decode("utf-8")
+
+
+"""
 async def ffmpeg_process(
     resolution_w, resolution_h, frame_rate, input_dir, output_dir, file_format, fps
 ):
@@ -65,6 +122,59 @@ async def ffmpeg_process(
             shell=True,
         )
     return process_log.communicate()[1].decode("utf-8")
+"""
+
+
+def ffmpeg_process_qprocess(
+    self,
+    resolution_w,
+    resolution_h,
+    frame_rate,
+    input_dir,
+    output_dir,
+    file_format,
+    fps,
+):
+    log_location = f"ErrorLog/ffmpeg_{date}.txt"
+    if not check_pyinstaller():
+        self.p = QProcess()
+        self.p.setProcessChannelMode(QProcess.ForwardedChannels)
+        self.p.setStandardOutputFile(log_location)
+        self.p.start(
+            "python",
+            [
+                "runner.py",
+                "--input",
+                input_dir,
+                "--output",
+                output_dir,
+                "--fps",
+                fps,
+                "--time",
+                f"{frame_rate}",
+                "--format",
+                file_format,
+            ],
+        )
+    else:
+        self.p = QProcess()
+        self.p.setProcessChannelMode(QProcess.ForwardedChannels)
+        self.p.setStandardOutputFile(log_location)
+        self.p.start(
+            "runner.exe",
+            [
+                "--input",
+                input_dir,
+                "--output",
+                output_dir,
+                "--fps",
+                fps,
+                "--time",
+                f"{frame_rate}",
+                "--format",
+                file_format,
+            ],
+        )
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -79,6 +189,8 @@ class MainWindow(QtWidgets.QMainWindow):
         # self.ui.Resolution_W.setText("1920")
         # self.ui.Resolution_H.setText("1080")
         # self.ui.ErrorLog.moveCursor(QtGui.QTextCursor.End)
+        self.ui.Resolution_W.setEnabled(False)
+        self.ui.Resolution_H.setEnabled(False)
         self.scrollbar = (
             self.ui.ErrorLog.verticalScrollBar()
         )  # the self.scrollbar is the same as your self.console_window
@@ -106,7 +218,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def open_file(self):
         output_dir = QtWidgets.QFileDialog.getSaveFileName(
-            self, "選擇輸出檔案", "", "MPEG-4 (*.mp4)"
+            self, "選擇輸出檔案", date, "MPEG-4 (*.mp4)"
         )
         if output_dir[0]:
             self.ui.OutputDir.setText(output_dir[0])
@@ -116,12 +228,12 @@ class MainWindow(QtWidgets.QMainWindow):
     def start(self):
         if self.ui.InputDir.text() == "":
             QtWidgets.QMessageBox.information(
-                self, "警告", "請選擇輸入資料夾", QtWidgets.QMessageBox.Close
+                self, "錯誤", "請選擇輸入資料夾", QtWidgets.QMessageBox.Close
             )
             return False
         if self.ui.OutputDir.text() == "":
             QtWidgets.QMessageBox.information(
-                self, "警告", "請選擇輸出檔案", QtWidgets.QMessageBox.Close
+                self, "錯誤", "請選擇輸出檔案", QtWidgets.QMessageBox.Close
             )
             return False
         """
@@ -135,7 +247,7 @@ class MainWindow(QtWidgets.QMainWindow):
         input_dir = self.ui.InputDir.text()
         if os.path.isdir(input_dir) is False:
             QtWidgets.QMessageBox.information(
-                self, "警告", "輸入資料夾不存在", QtWidgets.QMessageBox.Close
+                self, "錯誤", "輸入資料夾不存在", QtWidgets.QMessageBox.Close
             )
             return False
         output_dir = self.ui.OutputDir.text()
@@ -146,11 +258,12 @@ class MainWindow(QtWidgets.QMainWindow):
         file_format = self.ui.FileFormat.text()
         if file_format == "":
             QtWidgets.QMessageBox.information(
-                self, "警告", "請輸入檔案格式", QtWidgets.QMessageBox.Close
+                self, "錯誤", "請輸入檔案格式", QtWidgets.QMessageBox.Close
             )
             return False
         elif file_format.find(".") != -1:
             file_format = file_format.replace(".", "")
+        """
         if resolution_w and resolution_h:
             if resolution_w.isdigit() is False:
                 QtWidgets.QMessageBox.information(self, "警告", "解析度(寬)請輸入數字")
@@ -162,10 +275,11 @@ class MainWindow(QtWidgets.QMainWindow):
                 QtWidgets.QMessageBox.information(self, "警告", "解析度(寬)與(長)請輸入大於0的數字")
                 return False
             if int(resolution_w) != float(resolution_w) or int(resolution_h) != float(
-                resolution_h
+                    resolution_h
             ):
                 QtWidgets.QMessageBox.information(self, "警告", "解析度(寬)與(長)請輸入整數")
                 return False
+        """
         if os.path.isfile(output_dir) is True:
             if QtWidgets.QMessageBox.Yes == QtWidgets.QMessageBox.warning(
                 self,
@@ -183,20 +297,22 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.StartButton.setEnabled(False)
         self.ui.StartButton.setText("處理中...")
         try:
-            process_log_text = asyncio.run(
-                ffmpeg_process(
-                    resolution_w,
-                    resolution_h,
-                    frame_rate,
-                    input_dir,
-                    output_dir,
-                    file_format,
-                    fps,
-                )
+            ffmpeg_process_qprocess(
+                self,
+                resolution_w,
+                resolution_h,
+                frame_rate,
+                input_dir,
+                output_dir,
+                file_format,
+                fps,
             )
-            print(process_log_text)
-            self.ui.ErrorLog.setText(process_log_text)
-            self.ui.ErrorLog.moveCursor(QtGui.QTextCursor.End)
+            time.sleep(1)
+            """
+            with open(os.environ["TEMP"] + "\\ffmpeg_output\\ffmpeg_output.txt", "r") as f:
+                log = f.read()
+            self.ui.ErrorLog.setText(log)
+            """
         except:
             self.ui.ErrorLog.setText("處理失敗")
             QtWidgets.QMessageBox.information(
@@ -206,12 +322,12 @@ class MainWindow(QtWidgets.QMainWindow):
             print(error_traceback)
             if not os.path.isdir("ErrorLog"):
                 os.mkdir("ErrorLog")
-            date = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
             with open(
                 "ErrorLog/ErrorLog_{}.txt".format(date), "w", encoding="utf-8"
             ) as f:
                 f.write(error_traceback)
             self.ui.ErrorLog.setText(error_traceback)
+        self.ui.ErrorLog.moveCursor(QtGui.QTextCursor.End)
         self.ui.StartButton.setEnabled(True)
         self.ui.StartButton.setText("開始")
 
---
+++ runner.py
@@ -0,0 +1,75 @@
+from PySide2.QtGui import QFontDatabase
+from better_ffmpeg_progress import FfmpegProcess
+import os
+import sys
+import subprocess
+import argparse
+from PySide2 import QtWidgets
+# from PySide2.QtGui import QFontDatabase
+
+TEMP = os.environ.get("TEMP")
+TEMP = TEMP.replace("\\", "/")
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--input", help="Input Dir", required=True)
+parser.add_argument("--output", help="Output File", required=True)
+parser.add_argument("--time", help="Time per picture (sec)", required=True)
+parser.add_argument("--fps", help="FPS", required=True)
+parser.add_argument("--width", help="Width", required=False)
+parser.add_argument("--height", help="Height", required=False)
+parser.add_argument("--format", help="Format", required=True)
+
+input_dir = parser.parse_args().input
+file_format = parser.parse_args().format
+output_file = parser.parse_args().output
+fps = parser.parse_args().fps
+framerate = f"1/{parser.parse_args().time}"
+full_input_dir = f"{input_dir}/%03d.{file_format}"
+
+process = FfmpegProcess(
+    [
+        "ffmpeg.exe",
+        "-y",
+        "-framerate",
+        framerate,
+        "-i",
+        full_input_dir,
+        "-c:v",
+        "libx264",
+        "-pix_fmt",
+        "yuv420p",
+        "-vf",
+        f"fps={fps}",
+        output_file,
+    ]
+)
+
+
+def handle_progress_info(percentage, speed, eta, estimated_filesize):
+    print(f"{percentage}% ETA: {eta}")
+
+
+if not os.path.exists(f"{TEMP}/ffmpeg_output"):
+    os.makedirs(f"{TEMP}/ffmpeg_output")
+
+process.run(
+    progress_handler=handle_progress_info,
+    ffmpeg_output_file=f"{TEMP}/ffmpeg_output/ffmpeg_output.txt",
+)
+
+
+class MainWindow(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        # QFontDatabase.addApplicationFont("NotoSansTC-Regular.otf")
+        QtWidgets.QMessageBox.information(
+            self, "完成", f"轉檔完成\n請查看\n{output_file}", QtWidgets.QMessageBox.Ok
+        )
+        sys.exit()
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication()
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
